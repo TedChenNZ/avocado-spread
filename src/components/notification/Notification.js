5f90dd09a0312978be50a2ca29b0a4c7f6c6d5d2
gwyneth-bassett <gwyneth.bassett@eroad.com>
2018-04-27 10:15:43 +1200

    stylin'

--- src/components/notification/Notification.js
+++ src/components/notification/Notification.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styles from './styles.css';
 import Time from './Time.js';
+import { IconButton } from 'react-toolbox/lib/button';
 
 const getClassForPriority = notification => {
   switch (notification.severity) {
